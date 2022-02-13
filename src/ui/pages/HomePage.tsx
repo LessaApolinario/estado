85a76c29844ab001cb60aff85433296b536a4006
Lessa Apolinario <lessaapolinario@gmail.com>
2022-02-13 15:48:08 -0300

    HomePage component done

--- src/ui/pages/HomePage.tsx
+++ src/ui/pages/HomePage.tsx
@@ -1,9 +1,54 @@
+import { useState } from 'react'
+
+import Button from '../components/Button'
+
 import '../styles/pages/HomePage.css'
 
 function HomePage() {
+  const [number, setNumber] = useState(0)
+
+  function somar() {
+    setNumber(number + 1)
+  }
+
+  function subtrair() {
+    setNumber(number - 1)
+  }
+
+  function multiplicar() {
+    setNumber(number * 1)
+  }
+
+  function dividir() {
+    setNumber(number / 1)
+  }
+
   return (
-    <div>
-      <p>Testando</p>
+    <div className="counter-box">
+      <h1>Contador</h1>
+      <p>{number}</p>
+
+      <div className="btns">
+        <Button
+          text="Somar 1"
+          onClick={somar}
+        />
+
+        <Button
+          text="Subtrair 1"
+          onClick={subtrair}
+        />
+
+        <Button
+          text="Multiplicar por 1"
+          onClick={multiplicar}
+        />
+
+        <Button
+          text="Dividir por 1"
+          onClick={subtrair}
+        />
+      </div>
     </div>
   )
 }
